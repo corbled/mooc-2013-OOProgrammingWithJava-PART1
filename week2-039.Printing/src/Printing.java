2dbac1d502276b80c849e3dce4f452725ba90540
corbled <corentinbled@gmail.com>
2020-02-05 23:40:36 +0000

    Completed week2-039. Method calling other user created methods

--- week2-039.Printing/src/Printing.java
+++ week2-039.Printing/src/Printing.java
@@ -1,27 +1,8 @@
 public class Printing {
 
-    public static void printStars(int amount) {
-        // 39.1
-        // you can print one star with the command
-        // System.out.print("*");
-        // call this command amount times        
-    }
-
-    public static void printSquare(int sideSize) {
-        // 39.2
-    }
-
-    public static void printRectangle(int width, int height) {
-        // 39.3
-    }
-
-    public static void printTriangle(int size) {
-        // 39.4
-    }
-
     public static void main(String[] args) {
         // Tests do not use main, yo can write code here freely!
-        // if you have problems with tests, please try out first 
+        // if you have problems with tests, please try out first
         // here to see that the printout looks correct
 
         printStars(3);
@@ -32,6 +13,39 @@ public class Printing {
         System.out.println("\n---");
         printTriangle(3);
         System.out.println("\n---");
+    }
+
+    public static void printStars(int amount) {
+        int counter = 0;
+        while (counter < amount) {
+            System.out.print("*");
+            counter ++;
+        }
+        System.out.println();
+    }
+
+    public static void printSquare(int sideSize) {
+        int counter = 0;
+        while (counter < sideSize) {
+            printStars(sideSize);
+            counter ++;
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        int counter = 0;
+        while (counter < height) {
+            printStars(width);
+            counter ++;
+        }
+    }
+
+    public static void printTriangle(int size) {
+        int counter = 1;
+        while (counter <= size) {
+            printStars(counter);
+            counter ++;
+        }
     }
 
 }
