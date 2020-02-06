7ae43fab9ae68f74ba69a9dc86f81981ae7c4239
corbled <corentinbled@gmail.com>
2020-02-06 12:26:19 +0000

    Completed week2-041. Programming using user created methods. Random number guesser program using created guessing method.

--- week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -9,11 +9,30 @@ public class GuessingNumberGame {
         int numberDrawn = drawNumber();
 
         // program your solution here. Do not touch the above lines!
-        
+        guess(numberDrawn, reader);
     }
 
     // DO NOT MODIFY THIS!
     private static int drawNumber() {
         return new Random().nextInt(101);
+    }
+
+    public static void guess(int numberDrawn, Scanner reader) {
+        int guess, numGuesses = 0;
+
+        while (true) {
+            System.out.print("Guess a number: ");
+            guess = Integer.parseInt(reader.nextLine());
+            numGuesses ++;
+
+            if (guess > numberDrawn) {
+                System.out.println("The number is lesser, guesses made: " + numGuesses);
+            } else if (guess <  numberDrawn) {
+                System.out.println("The number is greater, guesses made: " + numGuesses);
+            } else {
+                System.out.println("Congratulations, your guess is correct!");
+                break;
+            }
+        }
     }
 }
