a12c9188a0ebb0aa02643b131623bcba562ef6b9
corbled <corentinbled@gmail.com>
2020-02-06 00:36:43 +0000

    Completed week2-040. Method calling other methods

--- week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -1,20 +1,5 @@
 public class PrintingLikeBoss {
 
-    // copy or rewrite the method of Assignment 39.1 here
-    public static void printStars(int amount) {
-    }
-
-    public static void printWhitespaces(int amount) {
-        // 40.1
-    }
-
-    public static void printTriangle(int size) {
-        // 40.2
-    }
-
-    public static void xmasTree(int height) {
-        // 40.3
-    }
 
     public static void main(String[] args) {
         // Tests do not use main, yo can write code here freely!
@@ -24,5 +9,61 @@ public class PrintingLikeBoss {
         xmasTree(4);
         System.out.println("---");
         xmasTree(10);
+    }
+
+    public static void printStars(int amount) {
+        int counter = 0;
+        while (counter < amount) {
+            System.out.print("*");
+            counter ++;
+        }
+        System.out.println();
+    }
+
+    public static void printWhitespaces(int amount) {
+        int counter = 0;
+        while (counter < amount) {
+            System.out.print(" ");
+            counter ++;
+        }
+
+    }
+
+    public static void printTriangle(int size) {
+        int white = size - 1, counter = 1;
+        while (counter <= size) {
+            printWhitespaces(white);
+            printStars(counter);
+
+            white --;
+            counter ++;
+        }
+    }
+
+    public static void xmasTree(int height) {
+        int width = height + (height - 1);
+        int white = width/2, counter = 1;
+
+        // Print foliage
+        while (white >= 0) {
+            printWhitespaces(white);
+            printStars(counter);
+
+            white --;
+            counter += 2;
+        }
+
+        // Print stump
+        white = (width/2) - 1;
+        int stump = 2;
+        while (stump > 0) {
+            printWhitespaces(white);
+            printStars(3);
+
+            stump --;
+        }
+
+
+
     }
 }
