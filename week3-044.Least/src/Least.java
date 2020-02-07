22e94bf36e536472210effe141f13a21d29b65af
corbled <corentinbled@gmail.com>
2020-02-07 13:51:33 +0000

    Completed week3-044 & week3-045. Passing parameters to methods and returning variables. Returning lowest of set of numbers (44), returning greatest of set of numbers (45).

--- week3-044.Least/src/Least.java
+++ week3-044.Least/src/Least.java
@@ -2,10 +2,10 @@
 public class Least {
 
     public static int least(int number1, int number2) {
-        // write program code here
-        // do not print anything inside the method
-        // method needs a return in the end
-        return -1;
+        if (number1 < number2) {
+            return number1;
+        }
+        return number2;
     }
 
     public static void main(String[] args) {
--- week3-045.Greatest/src/Greatest.java
+++ week3-045.Greatest/src/Greatest.java
@@ -2,8 +2,13 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
+        if (number1 > number2 && number1 > number3) {
+            return number1;
+        } else if (number2 > number1 && number2 > number3) {
+            return number2;
+        }
 
-        return -1;
+        return number3;
     }
 
     public static void main(String[] args) {
