fd09c72bdee8bb3967ee5115cf06eab90d09c3c9
corbled <corentinbled@gmail.com>
2020-02-10 22:39:47 +0000

    Completed week3-059 & week3-060. Exercises involving ordering, reversing and reshuffling arrayLists using the collections class.

--- week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -1,3 +1,6 @@
+
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -6,5 +9,31 @@ public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+
+        fillArray(reader, words);
+        Collections.reverse(words);
+        printArray(reader, words);
+    }
+
+    public static void fillArray(Scanner reader, ArrayList<String> words) {
+        String word = "";
+
+        while(true) {
+            System.out.println("Type a word: ");
+            word = reader.nextLine();
+
+            if (!word.isEmpty()) {
+                words.add(word);
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(Scanner reader, ArrayList<String> words) {
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
--- week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -1,4 +1,5 @@
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,5 +9,33 @@ public class WordsInAlphabeticalOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+
+        fillArrayList(reader, words);
+        Collections.sort(words);
+        printArrayList(reader, words);
+    }
+
+    public static void fillArrayList(Scanner reader, ArrayList<String> words) {
+        String word = "";
+
+        while (true) {
+            System.out.println("Type a word: ");
+            word = reader.nextLine();
+
+            if (!word.isEmpty()) {
+                words.add(word);
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void printArrayList(Scanner reader, ArrayList<String> words) {
+        System.out.println("You typed the following words: ");
+
+        for(String word : words) {
+            System.out.println(word);
+        }
     }
 }
