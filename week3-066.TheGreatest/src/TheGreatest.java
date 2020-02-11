a5f2e61b2ab24ac85bff7569101ecd67151d39f6
corbled <corentinbled@gmail.com>
2020-02-11 15:40:31 +0000

    Completed week3-065, 66, 67. Exercises involved using ArrayList as return value for method.

--- week3-066.TheGreatest/src/TheGreatest.java
+++ week3-066.TheGreatest/src/TheGreatest.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 
 public class TheGreatest {
-    public static int greatest(ArrayList<Integer> list) {
-        // write code here
-        return 0;
-    }
-
     public static void main(String[] args) {
         ArrayList<Integer> lista = new ArrayList<Integer>();
         lista.add(3);
         lista.add(2);
         lista.add(7);
         lista.add(2);
-        
+
         System.out.println("The greatest number is: " + greatest(lista));
+    }
+
+    public static int greatest(ArrayList<Integer> list) {
+        // write code here
+        int greatest = Integer.MIN_VALUE;
+        for(int num : list) {
+            if (num > greatest) {
+                greatest = num;
+            }
+        }
+        return greatest;
     }
 }
--- week3-067.TheVariance/src/Variance.java
+++ week3-067.TheVariance/src/Variance.java
@@ -2,28 +2,46 @@ import java.util.ArrayList;
 
 public class Variance {
     // Copy here sum from exercise 63 
-    public static int sum(ArrayList<Integer> list) {
-        return 0;
-    }
-    
-    // Copy here average from exercise 64 
-    public static double average(ArrayList<Integer> list) {
-        return 0;
-    }
+    // Copy here average from exercise 64
 
-    public static double variance(ArrayList<Integer> list) {
-        // write code here
-        return 0;
-    }
-    
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(3);
         list.add(2);
         list.add(7);
         list.add(2);
-        
+
         System.out.println("The variance is: " + variance(list));
+    }
+    public static int sum(ArrayList<Integer> list) {
+        int sum = 0;
+        for(int num : list) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static double average(ArrayList<Integer> list) {
+        double average = ((double)sum(list))/list.size();
+        return average;
+    }
+
+    public static double variance(ArrayList<Integer> list) {
+        // write code here
+        double average = average(list);
+        System.out.println("average = " + average);
+
+        ArrayList<Double> sqrdVals = new ArrayList<Double>();
+        for(double num : list) {
+            num -= average;
+            sqrdVals.add(Math.pow((double)num, 2.0));
+        }
+
+        double sumSqrdVals = 0;
+        for(double num : sqrdVals) {
+            sumSqrdVals += num;
+        }
+        return sumSqrdVals/(list.size() -1);
     }
 
 }
