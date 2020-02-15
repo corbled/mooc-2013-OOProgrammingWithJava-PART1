2eb4b3f8fea28d4afe4e6fe888eb6fc59e1f7178
corbled <corentinbled@gmail.com>
2020-02-15 21:52:30 +0000

    Completed week3-068 & week3-069. Exercises involved using truth values/ booleans in methods and as return value.

--- week3-069.Palindrome/src/Palindromi.java
+++ week3-069.Palindrome/src/Palindromi.java
@@ -2,20 +2,37 @@ import java.util.Scanner;
 
 public class Palindromi {
 
-    public static boolean palindrome(String text) {
-        // write code here
-        return false;
-    }
-
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         System.out.println("Type a text: ");
-        String text = reader.nextLine();    
+        String text = reader.nextLine();
         if (palindrome(text)) {
             System.out.println("The text is a palindrome!");
         } else {
             System.out.println("The text is not a palindrome!");
         }
+    }
+
+    public static boolean palindrome(String text) {
+        String reverse = reverse(text);
+
+        if (text.equals(reverse)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String reverse(String text) {
+        String reverse = "";
+        int lastIndex = text.length() - 1;
+
+        while (reverse.length() != text.length()) {
+            reverse += text.charAt(lastIndex);
+            lastIndex --;
+        }
+
+        return reverse;
     }
 }
